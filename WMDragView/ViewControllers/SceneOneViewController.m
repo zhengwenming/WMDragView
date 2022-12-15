18e12586744ab5b958df7bd54f22069004df4b29
文明 <740747055@qq.com>
2022-12-15 23:18:55 +0800

    添加全局浮窗

--- WMDragView/ViewControllers/SceneOneViewController.m
+++ WMDragView/ViewControllers/SceneOneViewController.m
@@ -8,8 +8,7 @@
 
 #import "SceneOneViewController.h"
 #import "Masonry.h"
-#import "CustomerView.h"
-
+#import "AppDelegate.h"
 @interface SceneOneViewController ()
 
 @end
@@ -18,46 +17,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.navigationItem.title = @"全局浮窗";
+    
+    
+    WMDragView *logoView = [[WMDragView alloc] init];
+    logoView.imageView.image = [UIImage imageNamed:@"logo1024"];
+    [[UIApplication sharedApplication].delegate.window addSubview:logoView];
 
-    self.navigationItem.title = @"又回到最初的起点";
-    
-    
-//    WMDragView *orangeView = [[WMDragView alloc] init];
-//    orangeView.imageView.image = [UIImage imageNamed:@"logo1024"];
-//    orangeView.backgroundColor = [UIColor orangeColor];
-//    [self.view addSubview:orangeView];
-//
-//
-//
-//    [orangeView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.center.equalTo(self.view);
-//        make.size.mas_equalTo(CGSizeMake(80, 80));
-//    }];
-//
-//    orangeView.clickDragViewBlock = ^(WMDragView *dragView){
-//
-//
-//    };
-//
-//
-//    orangeView.endDragBlock = ^(WMDragView *dragView) {
-//
-//
-//
-//        };
 
-    
-    
-    
-    
-    CustomerView *cv = [[CustomerView alloc] initWithFrame:CGRectMake(0, 0, 180, 80)];
-    [self.view addSubview:cv];
 
-    cv.center = self.view.center;
-    
-    
+    [logoView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(logoView.superview);
+        make.size.mas_equalTo(CGSizeMake(80, 80));
+    }];
+
+    logoView.clickDragViewBlock = ^(WMDragView *dragView){
+
+
+    };
+
+    logoView.duringDragBlock = ^(WMDragView *dragView) {
+    };
+
+    logoView.endDragBlock = ^(WMDragView *dragView) {
+
+    };
 }
-
-
-
 @end
