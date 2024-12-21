54618c9ed02abc0988acab27fcb9971856ad7d67
wenming <740747055@qq.com>
2024-12-20 23:26:09 -0800

    fixed bug:window上的dragView在跳转VC时被复位frame
    注意,添加到window上的全局dragView不建议使用masonry添加约束，否则会在新的其他VC加载的时候，dragView被复位到初始化位置

--- WMDragView/ViewControllers/SceneOneViewController.m
+++ WMDragView/ViewControllers/SceneOneViewController.m
@@ -15,21 +15,26 @@
 
 @implementation SceneOneViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"全局浮窗";
     
     
-    WMDragView *logoView = [[WMDragView alloc] init];
+    WMDragView *logoView = [[WMDragView alloc] initWithFrame:CGRectMake(0, 0, 80, 80)];
     logoView.imageView.image = [UIImage imageNamed:@"logo1024"];
     [[UIApplication sharedApplication].delegate.window addSubview:logoView];
+    logoView.center = logoView.superview.center;
 
 
 
-    [logoView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.center.equalTo(logoView.superview);
-        make.size.mas_equalTo(CGSizeMake(80, 80));
-    }];
+
+#pragma mark !!!:  注意,添加到window上的全局dragView不建议使用masonry添加约束，否则会在新的其他VC加载的时候，dragView被复位到初始化位置。
+    
+//    [logoView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.center.equalTo(logoView.superview);
+//        make.size.mas_equalTo(CGSizeMake(80, 80));
+//    }];
 
     logoView.clickDragViewBlock = ^(WMDragView *dragView){
 
